3f8cdd6c1e379e1ffee96e1ea417297b16c0a50f
DESKTOP-F940KKR\Marius <Marius@nereikia.lt>
2018-02-04 19:15:43 +0200

    Tab functionality for tab2 done

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-console.log('ar veikia?');
+//document.ready(function() {
+
+    
+    function tab2click() {
+    //presing a tab2
+    document.querySelector("#tab1Content").classList.add("d-none");
+    document.querySelector("#navigationTab1").classList.remove("active");
+    document.querySelector("#navigationTab2").classList.add("active");
+    }
+
+    document.querySelector("#navigationTab2").onclick = tab2click;
+
+
+//});
