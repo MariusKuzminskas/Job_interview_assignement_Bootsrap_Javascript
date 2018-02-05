a2ae9be779bd1974b8b4316c7baedcfd7e73b2c2
DESKTOP-F940KKR\Marius <Marius@nereikia.lt>
2018-02-05 09:40:25 +0200

    tab2 html css complete. tab link functionality complete

--- js/script.js
+++ js/script.js
@@ -1,14 +1,27 @@
 //document.ready(function() {
 
     
+    // Clicking on tabs functions and event listeners
     function tab2click() {
-    //presing a tab2
-    document.querySelector("#tab1Content").classList.add("d-none");
-    document.querySelector("#navigationTab1").classList.remove("active");
-    document.querySelector("#navigationTab2").classList.add("active");
+    //hiding tab1 showing tab2
+        document.querySelector("#tab1Content").classList.add("d-none");
+        document.querySelector("#tab2Content").classList.remove("d-none");
+        document.querySelector("#navigationTab1").classList.remove("active");
+        document.querySelector("#navigationTab2").classList.add("active");
     }
 
+    //listening for a click on tab2
     document.querySelector("#navigationTab2").onclick = tab2click;
+    
+    function tab1click() {
+    //hiding tab2 showing tab1
+        document.querySelector("#tab2Content").classList.add("d-none");
+        document.querySelector("#tab1Content").classList.remove("d-none");
+        document.querySelector("#navigationTab2").classList.remove("active");
+        document.querySelector("#navigationTab1").classList.add("active");
+    }
 
-
+    //listening for a click on tab1
+    document.querySelector("#navigationTab1").onclick = tab1click;
+    
 //});
